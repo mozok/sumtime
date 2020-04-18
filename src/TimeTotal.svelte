676812679f7620b2460cec1fb321b2fa51a52437
mozokevgen <mozokevgen@gmail.com>
2020-04-18 18:48:15 +0300

    styled TimeTotal component

--- src/TimeTotal.svelte
+++ src/TimeTotal.svelte
@@ -4,6 +4,9 @@
         hours = 0,
         minutes = 0;
 
+    /** 
+     * Chane total time on TimeSlots update
+    */
     const unsubscribe = timeSlots.subscribe(value => {
         let newDays = 0,
             newHours = 0,
@@ -13,15 +16,15 @@
             newHours += parseInt(timeSlot.hours) ? parseInt(timeSlot.hours) : 0;
             newMinutes += parseInt(timeSlot.minutes) ? parseInt(timeSlot.minutes) : 0;
         });
-        
+
         if (newMinutes >= 60) {
-            let additionalHours = Math.floor(newMinutes/60); 
+            let additionalHours = Math.floor(newMinutes / 60);
             newHours += additionalHours;
             newMinutes -= additionalHours * 60;
         }
 
         if (newHours >= 24) {
-            let additionalDays = Math.floor(newHours/24);
+            let additionalDays = Math.floor(newHours / 24);
             newDays += additionalDays;
             newHours -= additionalDays * 24;
         }
@@ -29,18 +32,45 @@
         days = newDays;
         hours = newHours;
         minutes = newMinutes;
-	});
+    });
 </script>
 
-<div id="days">
-    <span>Days</span>
-    <p>{days}</p>
+<div class="container">
+    <div class="column">
+        <span class="title">Days</span>
+        <div class="digit">{days}</div>
+    </div>
+    <div class="column">
+        <span class="title">Hours</span>
+        <div class="digit">{hours}</div>
+    </div>
+    <div class="column">
+        <span class="title">Minutes</span>
+        <div class="digit">{minutes}</div>
+    </div>
 </div>
-<div id="hours">
-    <span>Hours</span>
-    <p>{hours}</p>
-</div>
-<div id="minutes">
-    <span>Minutes</span>
-    <p>{minutes}</p>
-</div>
+
+
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+    }
+
+    .container .column {
+        margin: 10px;
+        width: 14vw;
+        height: 15vw;
+        background-color: #fff;
+        border-radius: 1vw;
+        font-family: Oswald, sans-serif;
+    }
+
+    .container .column .title {
+        font-size: 2vw;
+    }
+
+    .container .column .digit {
+        font-size: 10vw;
+    }
+</style>
